13b8b5b13b82813f55ea2463dc86479ec38fc3d1
Faalukk <zelacer.tv@gmail.com>
2024-03-26 04:27:02 +0700

    ddd

---
+++ laravel/app/Http/Controllers/RoomController.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductRealModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
+
+class RoomController extends Controller
+{
+    public function index()
+    {
+        $emp = DB::table('Room')->get();
+        return view('Main',compact('emp'));
+    }
+    
+    
+}
